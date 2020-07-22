776e7aaa28ec9415f908dd3fc5f82be692f3ca56
Abhishek Srivastav <abhisrivastav551@gmail.com>
2020-07-22 14:23:47 +0530

    3. Create Tasks

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -17,7 +17,7 @@ contract('TodoList', (accounts) => {
     const taskCount = await this.todoList.taskCount()
     const task = await this.todoList.tasks(taskCount)
     assert.equal(task.id.toNumber(), taskCount.toNumber())
-    assert.equal(task.content, 'Check out dappuniversity.com')
+    assert.equal(task.content, 'Check out AbhiSri7 at github')
     assert.equal(task.completed, false)
     assert.equal(taskCount.toNumber(), 1)
   })
@@ -32,13 +32,13 @@ contract('TodoList', (accounts) => {
     assert.equal(event.completed, false)
   })
 
-  it('toggles task completion', async () => {
-    const result = await this.todoList.toggleCompleted(1)
-    const task = await this.todoList.tasks(1)
-    assert.equal(task.completed, true)
-    const event = result.logs[0].args
-    assert.equal(event.id.toNumber(), 1)
-    assert.equal(event.completed, true)
-  })
+  // it('toggles task completion', async () => {
+  //   const result = await this.todoList.toggleCompleted(1)
+  //   const task = await this.todoList.tasks(1)
+  //   assert.equal(task.completed, true)
+  //   const event = result.logs[0].args
+  //   assert.equal(event.id.toNumber(), 1)
+  //   assert.equal(event.completed, true)
+  // })
 
 })
